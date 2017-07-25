0f04ed49350f7b6d4b984cc2dc95fc5e5f292e89
Privezentsev Yura <privezentsev.yura@icloud.com>
2017-07-25 15:32:30 +0300

    Add FileTemplate implementation

--- gener/gener/Common/Categories/FIleTemplate.h
+++ gener/gener/Common/Categories/FIleTemplate.h
@@ -8,6 +8,30 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ Object representation of JSON file template with format:
+ 
+    {
+        name: "$module_name$ViewInput",
+        extension: ".m",
+        destination_path: "view",
+        template_path: "../path"
+    }
+ 
+ */
 @interface FIleTemplate : NSObject
+
+@property (strong, nonatomic) NSString *nameTemplate;
+@property (strong, nonatomic) NSString *extension;
+@property (strong, nonatomic) NSURL *destinationPath;
+@property (strong, nonatomic) NSURL *templatePath;
+
+/**
+ Initializes object with JSON dictionary of specified format
+
+ @param json dictionary with specified format
+ @return instance of FIleTemplate
+ */
+- (instancetype)initWithJSON:(NSDictionary *)json;
 
 @end
--- gener/gener/Common/Categories/FIleTemplate.m
+++ gener/gener/Common/Categories/FIleTemplate.m
@@ -10,4 +10,22 @@
 
 @implementation FIleTemplate
 
+- (instancetype)initWithJSON:(NSDictionary *)json {
+    self = [super init];
+    
+    if (self) {
+        
+        _nameTemplate = json[@"name"];
+        _extension = json[@"extension"];
+        
+        NSURL *destination = [NSURL URLWithString:json[@"destination_path"]];
+        NSURL *tempaltePath = [NSURL URLWithString:json[@"template_path"]];
+        
+        _destinationPath = destination;
+        _templatePath = tempaltePath;
+        
+    }
+    return self;
+}
+
 @end
