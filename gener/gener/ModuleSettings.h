95e68e97314845c4147d3587ce7a9dd7a390b2cd
Privezentsev Yura <privezentsev.yura@icloud.com>
2017-07-25 18:12:00 +0300

    add ModuleSettings, testTemplate.txt and template.json

---
+++ gener/gener/ModuleSettings.h
@@ -0,0 +1,13 @@
+//
+//  ModuleSettings.h
+//  gener
+//
+//  Created by Privezentsev Yury on 25.07.17.
+//  Copyright © 2017 GNR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ModuleSettings : NSObject
+
+@end
---
+++ gener/gener/ModuleSettings.m
@@ -0,0 +1,13 @@
+//
+//  ModuleSettings.m
+//  gener
+//
+//  Created by Privezentsev Yury on 25.07.17.
+//  Copyright © 2017 GNR. All rights reserved.
+//
+
+#import "ModuleSettings.h"
+
+@implementation ModuleSettings
+
+@end
