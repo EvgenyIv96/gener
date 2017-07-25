11ed1fd136acec198736f678c7d8343d1803d363
Privezentsev Yura <privezentsev.yura@icloud.com>
2017-07-25 18:17:06 +0300

    Add FileCreator implementation

--- gener/gener/Common/Categories/FileCreator.m
+++ gener/gener/Common/Categories/FileCreator.m
@@ -7,7 +7,36 @@
 //
 
 #import "FileCreator.h"
+#import "SourceFile.h"
 
 @implementation FileCreator
+
+#pragma mark - Public
+
+- (void)createFilesFromArray:(NSArray *)templates error:(NSError **)error {
+
+    for (SourceFile *file in templates) {
+        [self createFile:file error:nil];
+    }
+}
+
+#pragma mark - Private
+
+- (void)createFile:(SourceFile *)file error:(NSError *)error {
+
+    if (![[NSFileManager defaultManager] fileExistsAtPath:[file destinationPath]]) {
+
+        NSError *folderCreationError;
+
+        [[NSFileManager defaultManager] createDirectoryAtPath:[file destinationPath] withIntermediateDirectories:YES attributes:nil error:&folderCreationError];
+    }
+
+    NSString *filePath = [NSString stringWithFormat:@"%@/%@%@", [file destinationPath], [file name], [file extention]];
+    NSString *fileContent = [file content];
+    NSData *content = [fileContent dataUsingEncoding:NSUTF8StringEncoding];
+
+    [[NSFileManager defaultManager] createFileAtPath:filePath contents:content attributes:nil];
+
+}
 
 @end
